b07337b7d91341aa33e018a73eb9801e265cb71b
vagrant <vagrant@lcg.local>
2015-11-12 21:22:01 +0000

    Added db tweaks sql file

---
+++ db/lcg_db_tweaks_1.sql
@@ -0,0 +1,153 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: localhost    Database: lcg_db
+-- ------------------------------------------------------
+-- Server version	5.6.27-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `CardSkins`
+--
+
+DROP TABLE IF EXISTS `CardSkins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `CardSkins` (
+  `uuid` varchar(32) NOT NULL,
+  `Skins` mediumtext NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `UUID_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CardSkins`
+--
+
+LOCK TABLES `CardSkins` WRITE;
+/*!40000 ALTER TABLE `CardSkins` DISABLE KEYS */;
+/*!40000 ALTER TABLE `CardSkins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Cards`
+--
+
+DROP TABLE IF EXISTS `Cards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Cards` (
+  `uuid` varchar(32) NOT NULL,
+  `CardName` varchar(255) NOT NULL,
+  `DefaultImageFile` varchar(255) NOT NULL,
+  `Properties` mediumtext NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `UUID_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cards`
+--
+
+LOCK TABLES `Cards` WRITE;
+/*!40000 ALTER TABLE `Cards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cards` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Decks`
+--
+
+DROP TABLE IF EXISTS `Decks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Decks` (
+  `uuid` varchar(32) NOT NULL,
+  `DeckName` varchar(64) NOT NULL,
+  `Cards` mediumtext NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `UUID_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Decks`
+--
+
+LOCK TABLES `Decks` WRITE;
+/*!40000 ALTER TABLE `Decks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Decks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `RunePageVerification`
+--
+
+DROP TABLE IF EXISTS `RunePageVerification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `RunePageVerification` (
+  `uuid` varchar(32) NOT NULL,
+  `user_id` varchar(32) DEFAULT NULL,
+  `runepage_string` varchar(8) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `uuid_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `RunePageVerification`
+--
+
+LOCK TABLES `RunePageVerification` WRITE;
+/*!40000 ALTER TABLE `RunePageVerification` DISABLE KEYS */;
+/*!40000 ALTER TABLE `RunePageVerification` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `uuid` varchar(32) NOT NULL,
+  `summonerID` varchar(64) NOT NULL,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `UUID_UNIQUE` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES ('a87d590f51326677e70b0a7fd4657954','525738');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-12 13:21:03
