d6d3a01a3e617b2d7059a00e423cf901c497c80f
Tony Wong <microtony@gmail.com>
2015-11-13 07:03:08 +0800

    socket server add champion name

--- socket/card.js
+++ socket/card.js
@@ -4,6 +4,7 @@ var Card = function(player, id, champname) {
   console.log(champname);
   this.id = 'card' + player + 'c' + id;
   var champ = champion[champname];
+  this.champion = champname;
   this.player = player;
   this.container = 'hand';
   this.health = champ.health;
