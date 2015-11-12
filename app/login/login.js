e6c1f03e15faa0e4273e5191030552e78c670d91
Salvador Holguin <sholguin@wilsontechnologies.us>
2015-11-11 20:55:56 -0800

    mock of login is done

--- app/login/login.js
+++ app/login/login.js
@@ -8,43 +8,73 @@ angular.module("LoginApp", [])
 			controller: "LoginCtrl"
 		})
 }])
+.factory("Login", function($q, $timeout, LoginMock){
+	//when testing
+	return LoginMock
+})
 .factory("LoginMock", function($q, $timeout){
 	return {
 		getRunepageKey: function(name){
 			var p = $q.defer();
 
 			//WHEN 200
-			// $timeout(function(){
-			// 	p.resolve("WXYZ");
-			// }, 500);
+			$timeout(function(){
+				p.resolve("WXYZ");
+			}, 500);
 
 			//WHEN 400
+			// $timeout(function(){
+			// 	p.reject(false);
+			// })
+
+			return p.promise;
+		},
+		//checks to see if the player has named their runepage to the following key
+		verifyAccount: function(name, key){
+			var p = $q.defer();
+
+			//WHEN 200
 			$timeout(function(){
-				p.reject(false);
+				p.resolve(true);
 			})
 
 			return p.promise;
 		}
 	}
 })
-.controller("LoginCtrl", ['$scope', '$log', 'LoginMock', function($scope, $log,  LoginMock){
+
+.controller("LoginCtrl", ['$scope', '$log', 'LoginMock', function($scope, $log,  Login){
 	//form object
 	$scope.form = {
 		name: "",
 		runepageKey: null,
-		notFound: false
+		notFound: null
 	}
 
 	//verifies the user name, returns a random generated string for the user to name a runepage as.
-	$scope.getRunepageKey = function(name){
+	$scope.getRunepageKey = function(){
 
-		LoginMock.getRunepageKey(name).then(function(result){
+		Login.getRunepageKey($scope.form.name).then(function(result){
 			$scope.form.runepageKey = result;
+			$scope.form.notFound = false;
 		})
 		//when an user is not define
 		.catch(function(){
 			$log.error("fail");
 			$scope.form.runepageKey = null;
+			$scope.form.notFound = true;
+		})
+	};
+
+	$scope.verifyAccount = function(){
+		//Login
+		Login.verifyAccount($scope.form.name, $scope.form.runepageKey).then(function(result){
+			//they are logged in, send them to the home page
+			$log.debug("Log in");
+		})
+		//fails
+		.catch(function(){
+
 		})
 	}
 }])
