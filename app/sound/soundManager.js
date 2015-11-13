4623089490ef751bfde3f95ae826b4457223fcd5
vagrant <vagrant@lcg.local>
2015-11-13 06:01:54 +0000

    added attack sound

--- app/sound/soundManager.js
+++ app/sound/soundManager.js
@@ -13,6 +13,8 @@ function backgroundMusic(){
 function soundController(soundType,id){
     if(!mute) {
         if(soundType == 'attack') {
+            console.log("playing attack card sound");
+            console.log(id);
             var sound = new buzz.sound('/rghack2015/app/sound/sounds/attack/' + id + '.ogg',{volume:50});
             sound.play();
         }
