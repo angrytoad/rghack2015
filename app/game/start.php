6e4c21802dd7ba13702f83ed03bd0174c34fb927
Salvador Holguin <sholguin@wilsontechnologies.us>
2015-11-12 17:01:20 -0800

    fix merge issues on start.php

--- app/game/start.php
+++ app/game/start.php
@@ -6,14 +6,10 @@
 	ini_set( "display_errors", 1 );
 	
 	$address = 'http://52.32.183.170:3001/init';
-<<<<<<< HEAD
+
 	$input = json_decode(file_get_contents("php://input"));
 
 	$data = array('deck' => $deck, "name" => $input->name, "id" => $input->id);
-=======
-	
-	$data = array('deck0' => $deck, 'deck1' => $deck);
->>>>>>> 32fc77634af79fdbda4b24393e71b2ade71a6922
 
 	$options = array(
     	'http' => array(
@@ -26,16 +22,10 @@
 	$context = stream_context_create($options);
 	
 	$result = file_get_contents($address, false, $context);
-	
-<<<<<<< HEAD
+
 	if ($result === "0" || $result === "1") {
 
 		echo $result;
-=======
-	if ($result === 'game initialized') {
-		
-			
->>>>>>> 32fc77634af79fdbda4b24393e71b2ade71a6922
 
 	} else {
 		
